eb99557cc8516440bcf7837df3fa20b3649c67b4
Horacio Gricel BORRAZ GONZALEZ <horacio.borraz@student.bodwell.edu>
2018-01-20 12:27:37 -0800

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,47 @@
+import static java.lang.System.console;
+import static java.lang.System.setOut;
+
+//todo:*** Execises to practice the basic syntax*/
+public class Basics {
+
+    public static void main(String[] args) {
+        // TODO: Write a Java program to print the sum of two numbers.
+        int maldad, evil;
+        maldad = 9;
+        evil = 3;
+        int cilantro = maldad + evil;
+        System.out.println("\n\nCilantro: = " + cilantro);
+
+        // TODO: Write a Java program to divide two numbers and print on the screen. Use both integers and doubles.
+        int sangre, muerte;
+        sangre = 10;
+        muerte = 2;
+        int destruccion = sangre / muerte;
+        System.out.println("\n\nDestruction = " + destruccion);
+        // TODO: Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+        System.out.print("At this point this is a reminder that\nsangre = " + sangre + "\nmaldad = " + maldad + "\nSo...\n\n");
+        destruccion = sangre + maldad;
+        System.out.println(destruccion);
+        destruccion = sangre / maldad;
+        System.out.println(destruccion);
+        destruccion = sangre * maldad;
+        System.out.println(destruccion);
+        destruccion = sangre - maldad;
+        System.out.println(destruccion);
+
+
+
+        // TODO: Write a Java program to swap two variables. Print the before and after switching.
+        boolean cawama;
+        boolean drunk;
+        cawama = false;
+        drunk = true;
+        System. out. print("\n\nCawama = " + cawama);
+        System. out. print("\nDrunk = " + drunk);
+        boolean malgo = cawama;
+        cawama = drunk;
+        drunk = malgo;
+        System. out. print("\n\nCawama = " + cawama);
+        System. out. print("\nDrunk = " + drunk);
+    }
+}
